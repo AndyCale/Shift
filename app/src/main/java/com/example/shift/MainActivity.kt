1fd963c99eff1a68b6a1c85a74b6b9c6a7134ea0
Александра Быковская <Aibykovs@mail.ru>
2024-02-16 22:28:36 +0700

    Fixed an application crash bug due to server not responding

--- app/src/main/java/com/example/shift/MainActivity.kt
+++ app/src/main/java/com/example/shift/MainActivity.kt
@@ -1,23 +1,16 @@
 package com.example.shift
 
-import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
 import android.content.Intent
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.shift.databinding.ActivityMainBinding
-import com.google.gson.JsonObject
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
 import java.net.URL
-import java.util.concurrent.Callable
-import java.util.concurrent.Executor
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 import kotlin.concurrent.thread
 
 
@@ -50,9 +43,18 @@ class MainActivity : AppCompatActivity() {
                 fullInform(5)
             }
             btnUpdate.setOnClickListener {
-                btnUpdate.isEnabled = false
+                tv1.text = "Загрузка..."
+                tv2.text = "Загрузка..."
+                tv3.text = "Загрузка..."
+                tv4.text = "Загрузка..."
+                tv5.text = "Загрузка..."
+                imgV1.setImageResource(R.drawable.pic_loading)
+                imgV2.setImageResource(R.drawable.pic_loading)
+                imgV3.setImageResource(R.drawable.pic_loading)
+                imgV4.setImageResource(R.drawable.pic_loading)
+                imgV5.setImageResource(R.drawable.pic_loading)
+
                 new_people()
-                btnUpdate.isEnabled = true
             }
         }
 
@@ -71,49 +73,71 @@ class MainActivity : AppCompatActivity() {
 
         var inform: String = ""
         getInfo {
-            inform = "Name: ${it[0]} ${it[1]}\nAddress: ${it[2]} ${it[3]}\nPhone: ${it[4]}";
-            binding.tv1.text = inform
-            Picasso.get().load(it[5]).into(binding.imgV1)
-        }
-
-        // Phone: ${it[4]}
-        getInfo {
-            inform = "Name: ${it[0]} ${it[1]}\nAddress: ${it[2]} ${it[3]}\nPhone: ${it[4]}";
-            binding.tv2.text = inform
-            Picasso.get().load(it[5]).into(binding.imgV2)
+            if (it.isNotEmpty()) {
+                inform = "Name: ${it[0]} ${it[1]}\nAddress: ${it[2]} ${it[3]}\nPhone: ${it[4]}";
+                binding.tv1.text = inform
+                Picasso.get().load(it[5]).into(binding.imgV1)
+            }
         }
 
         getInfo {
-            inform = "Name: ${it[0]} ${it[1]}\nAddress: ${it[2]} ${it[3]}\nPhone: ${it[4]}";
-            binding.tv3.text = inform
-            Picasso.get().load(it[5]).into(binding.imgV3)
+            if (it.isNotEmpty()) {
+                inform = "Name: ${it[0]} ${it[1]}\nAddress: ${it[2]} ${it[3]}\nPhone: ${it[4]}";
+                binding.tv2.text = inform
+                Picasso.get().load(it[5]).into(binding.imgV2)
+            }
         }
 
         getInfo {
-            inform = "Name: ${it[0]} ${it[1]}\nAddress: ${it[2]} ${it[3]}\nPhone: ${it[4]}";
-            binding.tv4.text = inform
-            Picasso.get().load(it[5]).into(binding.imgV4)
+            if (it.isNotEmpty()) {
+                inform = "Name: ${it[0]} ${it[1]}\nAddress: ${it[2]} ${it[3]}\nPhone: ${it[4]}";
+                binding.tv3.text = inform
+                Picasso.get().load(it[5]).into(binding.imgV3)
+            }
         }
 
         getInfo {
-            inform = "Name: ${it[0]} ${it[1]}\nAddress: ${it[2]} ${it[3]}\nPhone: ${it[4]}";
-            binding.tv5.text = inform
-            Picasso.get().load(it[5]).into(binding.imgV5)
+            if (it.isNotEmpty()) {
+                inform = "Name: ${it[0]} ${it[1]}\nAddress: ${it[2]} ${it[3]}\nPhone: ${it[4]}";
+                binding.tv4.text = inform
+                Picasso.get().load(it[5]).into(binding.imgV4)
+            }
+        }
+
+        getInfo {
+            if (it.isNotEmpty()) {
+                inform = "Name: ${it[0]} ${it[1]}\nAddress: ${it[2]} ${it[3]}\nPhone: ${it[4]}";
+                binding.tv5.text = inform
+                Picasso.get().load(it[5]).into(binding.imgV5)
+            }
         }
     }
 
     private fun getInfo(callback : (List<String>) -> Unit) {
+        var flag = false
         thread {
-            var request = JSONObject(URL("https://randomuser.me/api/").readText())
-            request = request.getJSONArray("results")[0] as JSONObject
-            val name = request.getJSONObject("name")
-            val address = request.getJSONObject("location").getJSONObject("street")
-            val pic = request.getJSONObject("picture").getString("medium")
-            handler.post {
-                callback.invoke(listOf(name.getString("first"), name.getString("last"),
-                    address.getString("name"), address.getString("number"),
-                            request.getString("phone"), pic))
+            while (!flag) {
+                try {
+                    var request = JSONObject(URL("https://randomuser.me/api/").readText())
+                    request = request.getJSONArray("results")[0] as JSONObject
+                    val name = request.getJSONObject("name")
+                    val address = request.getJSONObject("location").getJSONObject("street")
+                    val pic = request.getJSONObject("picture").getString("medium")
+                    flag = true
+                    handler.post {
+                        callback.invoke(
+                            listOf(
+                                name.getString("first"), name.getString("last"),
+                                address.getString("name"), address.getString("number"),
+                                request.getString("phone"), pic
+                            )
+                        )
+                    }
+                } catch (err: java.io.FileNotFoundException) {
+                    Log.i(TAG, "FileNotFoundException")
+                    handler.post { listOf<String>() }
+                }
             }
         }
     }
-    }
+}
---
+++ app/src/main/java/com/example/shift/db/dbNameClass.kt
@@ -0,0 +1,20 @@
+package com.example.shift.db
+
+import android.provider.BaseColumns
+
+object dbNameClass : BaseColumns {
+    const val TABLE_NAME = "table"
+    const val COLUMN_NAME_NAME = "name"
+    const val COLUMN_NAME_TITLE = "title"
+    const val COLUMN_NAME_PHONE = "phone"
+    const val COLUMN_NAME_EMAIL = "email"
+    const val COLUMN_NAME_ADDRESS = "address"
+    const val COLUMN_NAME_GENDER = "gender"
+    const val COLUMN_NAME_DOB = "dob"
+    const val COLUMN_NAME_AGE = "age"
+    const val COLUMN_NAME_PICTURE = "picture"
+    const val COLUMN_NAME_POSTCODE = "postcode"
+    const val COLUMN_NAME_COORDINATES = "coordinates"
+    const val COLUMN_NAME_OFFSET = "offset"
+    const val COLUMN_NAME_USERNAME = "username"
+}
